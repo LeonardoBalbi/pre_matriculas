f795b6861fb6d514d67feec8ac7d023def3b4ec3
Leonardo Balbi <leonardocbalbi@gmail.com>
2026-05-22 13:15:03 -0300

    teste

--- storage/framework/views/a8c170713ab66626a98f807240a546bf.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes->class(['fi-dropdown-list'])); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH D:\app_filament5_inicio\app\vendor\filament\support\resources\views/components/dropdown/list/index.blade.php ENDPATH**/ ?>
